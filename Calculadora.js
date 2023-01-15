f81c4a54a38571840ae816b5fff0e216b3ca4aaa
Juan Marcos Gonzalez <gonzalezromojm@hotmail.com>
2023-01-15 00:55:21 -0600

    Practica terminada

--- Calculadora.js
+++ Calculadora.js
@@ -1,27 +1,41 @@
 console.log("Aqui inicia mi programa");
 //Practicando funciones flecha
-const multiplicacion = (numeroUno, numeroDos) => numeroUno*numeroDos;
-const division = (numeroUno , numeroDos) => numeroUno / numeroDos;
+const multiplicacion = (numeroUno, numeroDos) => numeroUno * numeroDos;
+const division = (numeroUno, numeroDos) => numeroUno / numeroDos;
 //Practicando funciones expresadas 
-const suma = function(numeroUno,numeroDos){
+const suma = function (numeroUno, numeroDos) {
     const result = numeroUno + numeroDos;
     console.log(result);
     return result;
 
 }
 
-const resta = function (numeroUno,numeroDos){
-const result = numeroUno - numeroDos;
-return result;
+const resta = function (numeroUno, numeroDos) {
+    const result = numeroUno - numeroDos;
+    return result;
 }
+/*
+ En esta funcion recibimos el valor de operacion que es dado por cada
+ uno de los botones de los operadores, tomamos el valor de los dos 
+ input y se transfroma a tipo numerico flotante
 
-function escogiendoOperacion(operacion)
-{
-    let primerNumero = parseInt(document.getElementById("primerNumero").value);
-    let segundoNumero = parseInt(document.getElementById("segundoNumero").value);
-    let valorResultado= operacion(primerNumero,segundoNumero);
-    if(isNaN(valorResultado))
-    alert("Vuelve a ingresar los numeros, resultado no valido");
-    else
-    document.getElementById("resultado").innerHTML = valorResultado;
+ 
+ */
+
+function escogiendoOperacion(operacion) {
+    let primerNumero = parseFloat(document.getElementById("primerNumero").value);
+    let segundoNumero = parseFloat(document.getElementById("segundoNumero").value);
+    let operador = toString(operacion);
+    /*Si los dos valores son correctos son llamadas las funciones
+    Se va a mostrar el resultado con dos decimales*/
+    if (primerNumero <= 10000000 && primerNumero >= -10000000
+        && segundoNumero <= 10000000 && segundoNumero >= -10000000) {
+        let valorResultado = operacion(primerNumero, segundoNumero);
+        if (isNaN(valorResultado))
+            document.getElementById("resultado").innerHTML = "No se puede dividir 0 entre 0";
+        else if (valorResultado == Infinity)
+            document.getElementById("resultado").innerHTML = "Infinito";
+        else
+            document.getElementById("resultado").innerHTML = valorResultado.toFixed(2);
+    }
 }
